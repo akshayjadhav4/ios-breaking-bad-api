43d4f42de0f5a435567b75b06a9e10c043da30a7
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-10-08 12:55:32 +0530

    created model struct

--- breakingBad/ContentView.swift
+++ breakingBad/ContentView.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+struct Character: Codable {
+    var char_id: Int
+    var name: String
+    var birthday: String
+    var occupation: [String]
+    var status: String
+    var appearance: [Int]
+    var nickname: String
+    var portrayed: String
+    var img: String
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
