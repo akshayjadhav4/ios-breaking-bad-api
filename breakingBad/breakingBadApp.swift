f45626e37a1824ad222e53d9d63d6b5e87ae76b8
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-10-08 12:46:31 +0530

    Initial Commit

---
+++ breakingBad/breakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakingBadApp.swift
+//  breakingBad
+//
+//  Created by Akshay Anil Jadhav on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct breakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
